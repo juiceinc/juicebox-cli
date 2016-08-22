9e50165e9f0023836bffe839b7ec1a5c5222c716
Jason Myers <jason@jasonamyers.com>
2016-08-22 11:10:35 -0500

    Adding test framework and circle support
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- setup.py
+++ setup.py
@@ -32,6 +32,11 @@ setup(
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2'
+          'Programming Language :: Python :: 2.7'
+          'Programming Language :: Python :: 3'
+          'Programming Language :: Python :: 3.4'
+          'Programming Language :: Python :: 3.5'
           'Topic :: Office/Business',
-          ],
+    ],
 )
