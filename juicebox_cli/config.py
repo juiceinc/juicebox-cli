92554ce761c17a016c2b66baa4c42f8bbff2239e
Jason Myers <jason@jasonamyers.com>
2016-10-08 18:22:54 +0000

    Flipping hipaa to nthrive
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- juicebox_cli/config.py
+++ juicebox_cli/config.py
@@ -1,6 +1,7 @@
 NETRC_HOST_NAME = 'api.juiceboxdata.com'
 PUBLIC_API_URLS = {
-    'prod': 'https://api.juiceboxdata.com',
+    'prod': 'https://api.juiceboxdata.com',    
+    'nthriveprod': 'https://api.juiceboxdata.com',
     'dev': 'https://api-dev.juiceboxdata.com',
-    'hipaadev': 'https://api-dev.juiceboxdata.com',
+    'nthrivedev': 'https://api-dev.juiceboxdata.com',
 }
