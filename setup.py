2d9e403c1df3c593722feeedfef11f8899ac57a0
Christopher Armstrong <chris.armstrong@juiceanalytics.com>
2018-03-22 09:54:47 -0500

    bump version to 1.2.0.
    There were no changes in git from the last bump, but the PyPI 1.1.0
    release did not have the changes from GitHub 1.1.0.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ entry_points = {
 
 setup(
     name='juicebox-cli',
-    version='1.1.0',
+    version='1.2.0',
     description='Juicebox CLI',
     long_description=long_description,
     author='Juice Analytics',
