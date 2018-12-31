ab45a3fa15b3faec9ff2e6ce1328990992b0acf9
Christopher Armstrong <227068+radix@users.noreply.github.com>
2018-12-31 13:13:16 -0600

    update click to 7.0 and make the boto3 dependency in setup.py match the version in requirements.txt

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 requirements = [
-    'boto3==1.4.0',
+    'boto3==1.7.80',
     'certifi==2016.8.8',
-    'click==6.6',
+    'click==7.0',
     'requests==2.11.0'
 ]
 
